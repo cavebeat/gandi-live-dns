65f08836b7f9e6f8668932da3190f93f5716527b
David Ervideira <davidervideira@gmail.com>
2017-12-14 00:07:14 +0000

    Fix _ifconfig_ url in config example  - https://ifconfig.co does not give the ip only.    https://ifconfig.co/ip does.

--- src/example.config.py
+++ src/example.config.py
@@ -36,7 +36,7 @@ run your own external IP provider:
 + <?php $ip = $_SERVER['REMOTE_ADDR']; ?>
   <?php print $ip; ?>
 e.g. 
-+ https://ifconfig.co
++ https://ifconfig.co/ip
 + http://ifconfig.me/ip
 + http://whatismyip.akamai.com/
 + http://ipinfo.io/ip
