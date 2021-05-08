5e1b341826d200a736c5f07e4f4666e6141e04e2
Anthony Townsend <anthony@starcitygorup.us>
2021-05-08 07:33:39 -0400

    rewritten for multi-domain and multi-host update on a single container per dynamic host.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 DynDNS Updater for Gandi LiveDNS REST API
 
 Simple, just update config.py with your info and build and run the Dockerfile.
+
+```json
+domain_dict = {
+	'urbantech.live':['test1'],
+	'urbantechhub.io':['test1','test2'],
+	'cornellurban.tech':['test1','test2','test3']
+}
+```
--- gandi-live-dns.py
+++ gandi-live-dns.py
@@ -11,29 +11,29 @@ Created on 13 Aug 2017
 http://doc.livedns.gandi.net/ 
 http://doc.livedns.gandi.net/#api-endpoint -> https://dns.gandi.net/api/v5/
 
+Rewrite for multi-domain
+Anthony Townsend
+2021 May
+
 '''
 
 import json, requests, argparse
 from config import config
 
 
-
-def get_dynip(ifconfig_provider):
-    ''' find out own IPv4 at home <-- this is the dynamic IP which changes more or less frequently
-    similar to curl ifconfig.me/ip, see example.config.py for details to ifconfig providers 
-    ''' 
+def get_myip(ifconfig_provider):
     r = requests.get(ifconfig_provider)
-    print('Checking dynamic IP: ', r._content.decode("utf-8").strip('\n'))
+    print('\nChecking my IP: ', r._content.decode("utf-8").strip('\n'))
     return r.content.decode("utf-8").strip('\n')
 
-def get_uuid():
+def get_uuid(domain):
     ''' 
     find out ZONE UUID from domain
     Info on domain "DOMAIN"
     GET /domains/<DOMAIN>:
         
     '''
-    url = config.api_endpoint + '/domains/' + config.domain # todo where to start lopp over more than one domain?
+    url = config.api_endpoint + '/domains/' + domain
     u = requests.get(url, headers={"X-Api-Key": config.api_secret})
     json_object = json.loads(u._content)
     if u.status_code == 200:
@@ -43,7 +43,7 @@ def get_uuid():
         print((json_object['message']))
         exit()
 
-def get_dnsip(uuid):
+def get_dnsip(uuid,subdomain,fully_qualified):
     ''' find out IP from first Subdomain DNS-Record
     List all records with name "NAME" and type "TYPE" in the zone UUID
     GET /zones/<UUID>/records/<NAME>/<TYPE>:
@@ -52,17 +52,16 @@ def get_dnsip(uuid):
     the actual DNS Record IP
     '''
 
-    url = config.api_endpoint + '/zones/' + uuid + '/records/' + config.subdomains[0] + '/A'
+    url = config.api_endpoint + '/zones/' + uuid + '/records/' + subdomain + '/A'
     headers = {"X-Api-Key": config.api_secret}
     u = requests.get(url, headers=headers)
     if u.status_code == 200:
         json_object = json.loads(u._content)
-        print('Checking IP from DNS Record', config.subdomains[0], ':', json_object['rrset_values'][0])
-        return json_object['rrset_values'][0]
+        print('\nChecking IP from DNS Record', config.subdomains[0], ':', json_object['rrset_values'][0])
+        return True, json_object['rrset_values'][0]
     else:
-        print('Error: HTTP Status Code ', u.status_code, 'when trying to get IP from subdomain', config.subdomains[0])   
-        print(json_object['message'])
-        exit()
+        # print('ERROR ({}) Error: HTTP Status Code {} when trying to get IP — subdomain or domain probably doesnt exist.'.format(fully_qualified, u.status_code))
+        return False, None
 
 def update_records(uuid, dynIP, subdomain):
     ''' update DNS Records for Subdomains 
@@ -81,52 +80,54 @@ def update_records(uuid, dynIP, subdomain):
     json_object = json.loads(u._content)
 
     if u.status_code == 201:
-        print('Status Code:', u.status_code, ',', json_object['message'], ', IP updated for', subdomain)
+        # print('Status Code:', u.status_code, ',', json_object['message'], ', IP updated for', subdomain)
         return True
     else:
-        print('Error: HTTP Status Code ', u.status_code, 'when trying to update IP from subdomain', subdomain)   
+        print('Error: HTTP Status Code ', u.status_code, 'when trying to update IP from subdomain', subdomain)
         print(json_object['message'])
-        exit()
+        return
 
 
 
-def main(force_update, verbosity):
-
-    if verbosity:
-        print("verbosity turned on - not implemented by now")
-
-        
-    #get zone ID from Account
-    uuid = get_uuid()
-   
-    #compare dynIP and DNS IP 
-    dynIP = get_dynip(config.ifconfig)
-    dnsIP = get_dnsip(uuid)
-    
-    if force_update:
-        print("Going to update/create the DNS Records for the subdomains")
-        for sub in config.subdomains:# todo loop here ?
-            update_records(uuid, dynIP, sub)
-    else:
-        # bug script exits if first subdomain doesnt require an update—e.g. if we added to the config file and the IP didn't change (add new subdomains at the start of the list?)
-        if dynIP == dnsIP:
-            print("IP Address Match - no further action")
-        else:
-            print("IP Address Mismatch - going to update the DNS Records for the subdomains with new IP", dynIP)
-            for sub in config.subdomains:
-                update_records(uuid, dynIP, sub)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', help="increase output verbosity", action="store_true")
-    parser.add_argument('-f', '--force', help="force an update/create", action="store_true")
-    args = parser.parse_args()
+    print("Gandi DNS Updater v1.1 May 2021, by Anthony Townsend")
+
+    # parser = argparse.ArgumentParser()
+    # # parser.add_argument('-v', '--verbose', help="increase output verbosity", name="verbosity", action="store_true")
+    # parser.add_argument('-f', '--force', help="force an update/create", action="store_true")
+    # args = parser.parse_args()
         
-        
-    main(args.force, args.verbose)
+    # if verbosity:
+    #     print("Running in verbose mode.")
 
+    # get current IP
 
+    myIP = get_myip(config.ifconfig)
 
+    for domain,subdomains in config.domain_dict.items():
 
+        uuid = get_uuid(domain)
+        print ('\n***Using UUID {} for domain {}'.format(uuid,domain))
 
-    
+        for subdomain in subdomains:
+
+            fully_qualified = '.'.join((domain,subdomain))
+            exists, dnsIP=get_dnsip(uuid,subdomain,fully_qualified)
+
+            if exists == True:
+
+                # no update needed
+                if myIP == dnsIP:
+                    print ('NO UPDATE ({}) DNS IP {} matches Host IP {}'.format(fully_qualified,dnsIP,myIP))
+                #  update needed
+                elif myIP != dnsIP:
+                    try:
+                        update_records(uuid, myIP, subdomain)
+                        print('UPDATED ({}) DNS IP {} changed to match Host IP {}'.format(fully_qualified,dnsIP,myIP))
+                    except:
+                        print('ERROR ({}) Update failed'.format(fully_qualified))
+                        pass
+
+            elif exists == False:
+                print('ERROR ({}) Update failed, no DNS record for subdomain or domain.'.format(fully_qualified))
