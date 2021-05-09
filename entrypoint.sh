9639eb361cf1b6caaaa394e0504a72445b725cd9
Anthony Townsend <anthony@starcitygorup.us>
2021-05-09 15:49:19 -0400

    logging works, pushing image

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,8 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule for every minute, and follow the cron log
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-* * * * * cd /usr/src/app && python /usr/src/app/gandi-live-dns.py >> /var/log/cron.log 2>&1
+* * * * * cd /usr/src/app && python /usr/src/app/gandi-live-dns.py 2>/proc/1/fd/2 >&2
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
 cron -f
-touch /var/log/cron.log
-tail -f /var/log/cron.log
